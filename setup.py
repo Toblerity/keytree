b9562dc8264dfa2a94a2526274aa4eab69a1d241
sgillies <sgillies@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2008-05-01 16:52:10 +0000

    New project
    git-svn-id: http://atlantides.org/svn/pleiades/pleiades.keytree/trunk@1290 b08dbbb5-5418-0410-9893-8f6b77c4669c

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0'
+
+setup(name='pleiades.keytree',
+      version=version,
+      description="",
+      long_description="""\
+""",
+      classifiers=[],
+      keywords='',
+      author='Sean Gillies',
+      author_email='sgillies@frii.com',
+      url='',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
