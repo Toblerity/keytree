5c358099081e805b130b1b4419b4087b63db263b
Sean Gillies <sean.gillies@gmail.com>
2020-04-26 13:01:24 -0600

    Version 2.0.0 will be Python 3.6+ only

--- keytree/compat.py
+++ keytree/compat.py
@@ -1,7 +1,7 @@
 import warnings
 import sys
 
-if sys.version_info < (3,):
+if sys.version_info < (3, 6):
     warnings.warn(
         "Keytree will soon be Python 3 only. "
         "See this issue to know more: "
