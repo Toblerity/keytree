0692c48d13984555cb21a37176a3b98b82e08b88
Evan Derickson <3587185+dericke@users.noreply.github.com>
2020-05-19 23:10:56 -0700

    Correct usage of importorskip

--- keytree/tests/conftest.py
+++ keytree/tests/conftest.py
@@ -6,7 +6,7 @@ from xml.etree import ElementTree
 @pytest.fixture(params=["lxml", "ElementTree"])
 def etree(request):
     if request.param == "lxml":
-        etree = pytest.importorskip("lxml")
+        etree = pytest.importorskip("lxml.etree")
     else:
         etree = ElementTree
     return etree
